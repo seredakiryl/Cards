e712d6310cbe9b8903dba7c5196f68739af021c7
dsadas <seredakiryl@gmail.com>
2022-08-19 20:51:55 +0300

    add BrowserRouser And Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
-
+import { BrowserRouter, } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider >
   </BrowserRouter>
 
 );
