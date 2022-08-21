6c5385809127ac532becc6e29f2eb5572dab9e4f
dsadas <seredakiryl@gmail.com>
2022-08-21 16:19:38 +0300

    initial new branch

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,30 +1,26 @@
-import React from 'react';
-import {ButtonLogOut} from "./ButtonLogOut/ButtonLogOut";
+import { ButtonLogOut } from "./ButtonLogOut/ButtonLogOut";
 import s from './profile.module.css'
-import {Title} from "./Title/Title";
-import {Avatar} from "./Avatar/Avatar";
-import {EditableSpan} from "./EditableSpan/EditableSpan";
-import {Header} from "../Header/Header";
-import {BackArrow} from "../BackArrow/BackArrow";
+import { Title } from "./Title/Title";
+import { Avatar } from "./Avatar/Avatar";
+import { EditableSpan } from "./EditableSpan/EditableSpan";
+import { BackArrow } from "../BackArrow/BackArrow";
 
 
 export const Profile = () => {
-    return (
-        <div>
-            <BackArrow />
-            <div className={s.wrapper}>
-
-                <Title text={'Personal Information'}/>
-                <Avatar/>
-                <EditableSpan/>
-                <div>
-                    <span className={s.mail}>your-email@gmail.com</span>
-                </div>
-                <ButtonLogOut/>
-
+   return (
+      <div>
+         <BackArrow />
+         <div className={s.wrapper}>
+            <Title text={'Personal Information'} />
+            <Avatar />
+            <EditableSpan />
+            <div>
+               <span className={s.mail}>your-email@gmail.com</span>
             </div>
-        </div>
-        )
+            <ButtonLogOut />
+         </div>
+      </div>
+   )
 
 
 }
