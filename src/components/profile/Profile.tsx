67b8de94add5196c6efeb2cdb74f8d5ba2b66123
Anatoli Nikolaevich <tolya.tar@gmail.com>
2022-08-21 17:53:42 +0300

    fixed test

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,24 +1,23 @@
-import { ButtonLogOut } from './ButtonLogOut/ButtonLogOut'
+import {ButtonLogOut} from './ButtonLogOut/ButtonLogOut'
 import s from './profile.module.css'
-import { Title } from './Title/Title'
-import { Avatar } from './Avatar/Avatar'
-import { EditableSpan } from './EditableSpan/EditableSpan'
-import { BackArrow } from '../BackArrow/BackArrow'
+import {Title} from './Title/Title'
+import {Avatar} from './Avatar/Avatar'
+import {EditableSpan} from './EditableSpan/EditableSpan'
+import {BackArrow} from '../BackArrow/BackArrow'
 
 export const Profile = () => {
-  return (
-    <div>
-      <BackArrow />
-      <div className={s.wrapper}>
-        <Title text={'Personal Information'} />
-        <Avatar />
-        <EditableSpan />
+    return (
         <div>
-          <div>test git</div>
-          <span className={s.mail}>your-email@gmail.com</span>
+            <BackArrow/>
+            <div className={s.wrapper}>
+                <Title text={'Personal Information'}/>
+                <Avatar/>
+                <EditableSpan/>
+                <div>
+                    <span className={s.mail}>your-email@gmail.com</span>
+                </div>
+                <ButtonLogOut/>
+            </div>
         </div>
-        <ButtonLogOut />
-      </div>
-    </div>
-  )
+    )
 }
