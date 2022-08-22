1de969325486e73038e8609f8571d98d18e39e87
seredakiryl <seredakiryl@gmail.com>
2022-08-22 15:40:01 +0300

    add antDesign

--- src/components/auth/login/Login.tsx
+++ src/components/auth/login/Login.tsx
@@ -1,7 +1,9 @@
+import s from './Login.module.css'
+
 export const Login = () => {
   return (
-    <>
-      <div>login</div>
-    </>
+    <div>
+      <div className={s.login}></div>
+    </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+import 'antd/dist/antd.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <BrowserRouter>
