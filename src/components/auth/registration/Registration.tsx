f2b023d618b057f261bbc1b09b51bb3df7504437
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-08-23 20:15:03 +0200

    registration validation question

--- src/components/auth/registration/Registration.tsx
+++ src/components/auth/registration/Registration.tsx
@@ -6,6 +6,7 @@ import { useFormik } from 'formik'
 import { authAPI } from '../../../api/auth-api'
 import { setAppErrorAC } from '../../../store/app-reducer'
 import { useDispatch } from 'react-redux'
+import { FormikErrorType } from '../login/Login'
 
 export const Registration = () => {
   const dispatch = useDispatch()
@@ -13,7 +14,22 @@ export const Registration = () => {
     initialValues: {
       email: '',
       password: '',
+      confirm_password: '',
     },
+    validate: (values) => {
+      const errors: FormikErrorType = {}
+      if (!values.email) {
+        errors.email = 'Required'
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        console.log('validate', values)
+        errors.email = 'Invalid email address'
+      }
+
+      if (values.password !== values.confirm_password) {
+        errors.password = '!!!!'
+      }
+    },
+
     onSubmit: (values) => {
       authAPI
         .registration(values)
@@ -28,6 +44,10 @@ export const Registration = () => {
           <div className={s.headerSignUp}>Sign Up</div>
           <div className={s.headerEmail}>Email</div>
           <input type="email" className={s.inputEmail} {...formik.getFieldProps('email')} />
+          {formik.touched.email && formik.errors.email && (
+            <div style={{ color: 'red' }}>{formik.errors.email}</div>
+          )}
+
           <div className={s.headerPassword}>Password</div>
           <Input.Password
             className={s.inputPassword}
@@ -35,8 +55,17 @@ export const Registration = () => {
             {...formik.getFieldProps('password')}
           />
           <div className={s.headerConfirmPassword}>Confirm password</div>
-          <Input.Password className={s.inputConfirmPassword} />
+          <Input.Password
+            className={s.inputConfirmPassword}
+            type="confirm_password"
+            {...formik.getFieldProps('confirm_password')}
+          />
+          {formik.touched.email && formik.errors.password && (
+            <div style={{ color: 'red' }}>{formik.errors.password}</div>
+          )}
+
           <Button
+            disabled={true}
             htmlType="submit"
             type="primary"
             shape="round"
