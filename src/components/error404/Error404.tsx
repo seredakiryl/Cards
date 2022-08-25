10f7bec15d235e286b48d94a09674d195c961857
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-08-25 14:11:24 +0200

    update regestration

--- src/components/error404/Error404.tsx
+++ src/components/error404/Error404.tsx
@@ -1,7 +1,16 @@
+import { Result } from 'antd'
+import { Button } from 'antd/lib/radio'
+
 export const Error404 = () => {
   return (
     <>
-      <div>Error404</div>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={<Button type="primary">Back Home</Button>}
+      />
+      );
     </>
   )
 }
