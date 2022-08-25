0a63c9d654c583c602106f84ad046444938cc9fa
Anatoli Nikolaevich <tolya.tar@gmail.com>
2022-08-25 08:42:48 +0300

    header and start name fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import s from './Header.module.css'
 import { Avatar } from '../profile/Avatar/Avatar'
+import { useAppSelector } from '../../store/store'
+import { Button } from 'antd'
 // const logo = require('./../../assets/images/logo.png')
 
 export const Header = () => {
+  const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
   return (
     <div className={s.wrapper}>
       <div className={s.logo}></div>
-      <div className={s.userBlock}>
-        <div className={s.userName}>Name</div>
-        <div className={s.userAva}></div>
-      </div>
+      {isLoggedIn ? (
+        <div className={s.userBlock}>
+          <div className={s.userName}>Name</div>
+          <div className={s.userAva}></div>
+        </div>
+      ) : (
+        <Button type="primary" htmlType="submit" shape="round" className={s.Button}>
+          Sign in
+        </Button>
+      )}
     </div>
   )
 }
