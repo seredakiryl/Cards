709e961630aa7f10f6623c54dee12f13dbfdfd5e
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-08-25 23:02:39 +0200

    minor update

--- src/components/auth/registration/Registration.tsx
+++ src/components/auth/registration/Registration.tsx
@@ -39,14 +39,14 @@ export const Registration = () => {
         errors.password = 'Required'
       } else if (values.password.length < 5) {
         errors.password = 'Must be 15 characters or less'
-      } else if (values.password !== values.confirm_password) {
-        errors.password = 'password and confirm password have must be the same'
       }
 
       if (!values.confirm_password) {
         errors.confirm_password = 'Required'
       } else if (values.password.length < 5) {
         errors.confirm_password = 'Must be 15 characters or less'
+      } else if (values.password !== values.confirm_password) {
+        errors.confirm_password = 'password and confirm password have must be the same'
       }
       return errors
     },
@@ -61,62 +61,62 @@ export const Registration = () => {
 
   return (
     <div className={s.wrapper}>
-      <div className={s.container}>
-        <form onSubmit={formik.handleSubmit}>
-          <div className={s.headerSignUp}>Sign Up</div>
-          <div className={s.headerEmail}>Email</div>
-          <input type="email" className={s.inputEmail} {...formik.getFieldProps('email')} />
-          {formik.touched.email && formik.errors.email && (
-            <div style={{ color: 'red' }}>{formik.errors.email}</div>
-          )}
-          <div className={s.headerPassword}>Password</div>
-          <Input.Password
-            className={s.inputPassword}
-            type="password"
-            {...formik.getFieldProps('password')}
-          />
-          {formik.touched.password && formik.errors.password && (
-            <div style={{ color: 'red' }}>{formik.errors.password}</div>
-          )}
-          {/*вот этот допустим отработает, а вот ниже нет или наоборот*/}
-          <div className={s.headerConfirmPassword}>Confirm password</div>
-          <Input.Password
-            className={s.inputConfirmPassword}
-            type="confirm_password"
-            {...formik.getFieldProps('confirm_password')}
-          />
-          {formik.touched.confirm_password && formik.errors.confirm_password && (
-            <div style={{ color: 'red' }}>{formik.errors.confirm_password}</div>
-          )}
-          {/*а это не отработает, а вот ниже нет или наоборот, а хочется чтобы каждое поле обрабатывалось*/}
+      <form onSubmit={formik.handleSubmit} className={s.container}>
+        <h3 className={s.headerSignUp}>Sign Up</h3>
+        <label htmlFor={'email'} className={s.headerEmail}>
+          Email
+        </label>
+        <Input type="email" className={s.inputEmail} {...formik.getFieldProps('email')} />
+        {formik.touched.email && formik.errors.email && (
+          <div style={{ color: 'red' }}>{formik.errors.email}</div>
+        )}
+        <div className={s.headerPassword}>Password</div>
+        <Input.Password
+          className={s.inputPassword}
+          type="password"
+          {...formik.getFieldProps('password')}
+        />
+        {formik.touched.password && formik.errors.password && (
+          <div style={{ color: 'red' }}>{formik.errors.password}</div>
+        )}
+        {/*вот этот допустим отработает, а вот ниже нет или наоборот*/}
+        <div className={s.headerConfirmPassword}>Confirm password</div>
+        <Input.Password
+          className={s.inputConfirmPassword}
+          type="confirm_password"
+          {...formik.getFieldProps('confirm_password')}
+        />
+        {formik.touched.confirm_password && formik.errors.confirm_password && (
+          <div style={{ color: 'red' }}>{formik.errors.confirm_password}</div>
+        )}
+        {/*а это не отработает, а вот ниже нет или наоборот, а хочется чтобы каждое поле обрабатывалось*/}
 
-          {/*{formik.values.password === formik.values.confirm_password ? (*/}
-          {/*  <div>It`s work</div>*/}
-          {/*) : (*/}
-          {/*  <div>It`s work</div>*/}
-          {/*)}*/}
-          {/*это кусок тестовый, что так можно!!!
-           */}
+        {/*{formik.values.password === formik.values.confirm_password ? (*/}
+        {/*  <div>It`s work</div>*/}
+        {/*) : (*/}
+        {/*  <div>It`s work</div>*/}
+        {/*)}*/}
+        {/*это кусок тестовый, что так можно!!!
+         */}
 
-          <Button
-            disabled={formik.touched.password && formik.errors.password ? true : false}
-            htmlType="submit"
-            type="primary"
-            shape="round"
-            size={'large'}
-            className={s.buttonSingUp}
-          >
-            Sing up
-          </Button>
+        <Button
+          disabled={formik.touched.password && formik.errors.password ? true : false}
+          htmlType="submit"
+          type="primary"
+          shape="round"
+          size={'large'}
+          className={s.buttonSingUp}
+        >
+          Sing up
+        </Button>
 
-          <Button type="link" className={s.buttonHaveAccaunt}>
-            Already have an account?
-          </Button>
-          <Button type="link" className={s.buttonSingIn} onClick={rederectToLogin}>
-            Sign in
-          </Button>
-        </form>
-      </div>
+        <Button type="link" className={s.buttonHaveAccaunt}>
+          Already have an account?
+        </Button>
+        <Button type="link" className={s.buttonSingIn} onClick={rederectToLogin}>
+          Sign in
+        </Button>
+      </form>
     </div>
   )
 }
