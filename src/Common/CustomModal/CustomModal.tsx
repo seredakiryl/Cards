41bd07db75da9bd2cdcd29f68f6600fe61f42746
seredakiryl <seredakiryl@gmail.com>
2022-09-03 19:48:50 +0300

    eslinter fix

--- src/Common/CustomModal/CustomModal.tsx
+++ src/Common/CustomModal/CustomModal.tsx
@@ -1,37 +1,38 @@
-import { Button, Modal} from 'antd';
-import React, { ReactNode, useState} from 'react';
+import React, { ReactNode, useState } from 'react'
+
+import { Button, Modal } from 'antd'
 
 type CustomModalType = {
-    onCallback: (packName: string, isPrivate: boolean) => void
-    buttonName: string
-    children: ReactNode
-    packName: string
-    isPrivate: boolean
+  onCallback: (packName: string, isPrivate: boolean) => void
+  buttonName: string
+  children: ReactNode
+  packName: string
+  isPrivate: boolean
 }
 export const CustomModal = (props: CustomModalType) => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false)
 
-    const showModal = () => {
-        setIsModalVisible(true);
-    };
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
 
-    const handleOk = () => {
-        setIsModalVisible(false);
-        props.onCallback(props.packName, props.isPrivate)
-    };
+  const handleOk = () => {
+    setIsModalVisible(false)
+    props.onCallback(props.packName, props.isPrivate)
+  }
 
-    const handleCancel = () => {
-        setIsModalVisible(false);
-    };
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
 
-    return (
-        <>
-            <Button type="primary" onClick={showModal}>
-                {props.buttonName}
-            </Button>
-            <Modal title="Add new pack" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                {props.children}
-            </Modal>
-        </>
-    );
-};
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        {props.buttonName}
+      </Button>
+      <Modal title="Add new pack" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        {props.children}
+      </Modal>
+    </>
+  )
+}
--- src/Components/Packs/PacksHeader/PacksHeader.tsx
+++ src/Components/Packs/PacksHeader/PacksHeader.tsx
@@ -1,37 +1,39 @@
-import React, {ChangeEvent, useState} from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
-import { Button } from 'antd'
-
+import { CustomModal } from '../../../Common/CustomModal/CustomModal'
 import { addNewPack } from '../../../Store/packs-reducer'
 import { useAppDispatch } from '../../../Store/store'
 import { Title } from '../../Profile/Title/Title'
-import { CustomModal } from "../../../Common/CustomModal/CustomModal";
 
-import s from './PacksHeader.module.css';
-
+import s from './PacksHeader.module.css'
 
 export const PacksHeader = () => {
-
   const dispatch = useAppDispatch()
+
   const addNewPackHandler = () => {
     dispatch(addNewPack({ cardsPack: { name: 'the best pack ever!!!', private: false } }))
+  }
+  const [packName, setPackName] = useState('')
+  const [isPrivate, setIsPrivate] = useState(false)
 
-    const [packName, setPackName] = useState('')
-    const [isPrivate, setIsPrivate] = useState(false)
- 
   const onHandleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setPackName(e.currentTarget.value)
+    setPackName(e.currentTarget.value)
   }
   const setPrivatePack = (e: ChangeEvent<HTMLInputElement>) => {
-      setIsPrivate(e.currentTarget.checked)
-
+    setIsPrivate(e.currentTarget.checked)
   }
+
   return (
     <div className={s.wrapper}>
       <Title text={'Packs list'} />
-      <CustomModal buttonName={'Add new pack'} onCallback={addNewPackHandler} packName={packName} isPrivate={isPrivate}>
+      <CustomModal
+        buttonName={'Add new pack'}
+        onCallback={addNewPackHandler}
+        packName={packName}
+        isPrivate={isPrivate}
+      >
         <input type="text" onChange={onHandleChange} />
-        <input type="checkbox" onChange={setPrivatePack}/>
+        <input type="checkbox" onChange={setPrivatePack} />
       </CustomModal>
     </div>
   )
