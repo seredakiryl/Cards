cb7b0ba3f47d38b86aa27e8d5e31079301d006d3
Anatoli Nikolaevich <tolya.tar@gmail.com>
2022-09-03 10:54:53 +0300

    add new pack modal created

--- src/Components/Packs/PacksHeader/PacksHeader.tsx
+++ src/Components/Packs/PacksHeader/PacksHeader.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
+import React, {ChangeEvent, useState} from 'react'
 
 import { Button } from 'antd'
 
 import { packsAPI } from '../../../Api/packs-api'
 import { Title } from '../../Profile/Title/Title'
+import { CustomModal } from "../../../Common/CustomModal/CustomModal";
 
-import s from './PacksHeader.module.css'
+import s from './PacksHeader.module.css';
+
 
 export const PacksHeader = () => {
-  const addNewPackHandler = () => {
-    packsAPI.addPack({ cardsPack: { name: 'the best pack ever!!!', private: false } })
+    const [packName, setPackName] = useState('')
+    const [isPrivate, setIsPrivate] = useState(false)
+  const addNewPackHandler = (packName: string, isPrivate: boolean) => {
+    packsAPI.addPack({ cardsPack: { name: packName, private: isPrivate } })
   }
-
+  const onHandleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setPackName(e.currentTarget.value)
+  }
+  const setPrivatePack = (e: ChangeEvent<HTMLInputElement>) => {
+      setIsPrivate(e.currentTarget.checked)
+  }
   return (
     <div className={s.wrapper}>
       <Title text={'Packs list'} />
-      <Button
-        type="primary"
-        htmlType="submit"
-        shape="round"
-        className={s.button}
-        onClick={addNewPackHandler}
-      >
-        Add new pack
-      </Button>
+      <CustomModal buttonName={'Add new pack'} onCallback={addNewPackHandler} packName={packName} isPrivate={isPrivate}>
+        <input type="text" onChange={onHandleChange} />
+        <input type="checkbox" onChange={setPrivatePack}/>
+      </CustomModal>
     </div>
   )
 }
