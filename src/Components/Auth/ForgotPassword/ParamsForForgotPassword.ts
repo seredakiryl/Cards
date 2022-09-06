feed9e3147cbd65e0ff502ef36fcf532c314055e
Sobicus <83043123+Sobicus@users.noreply.github.com>
2022-09-06 18:09:03 +0200

    refactor cod

--- src/Components/Auth/ForgotPassword/ParamsForForgotPassword.ts
+++ src/Components/Auth/ForgotPassword/ParamsForForgotPassword.ts
@@ -1,6 +1,15 @@
 export let admin = 'test-front-admin <ai73a@yandex.by>'
-export let messageStyle = `<div style="background-color: red; padding: 15px">
-password recovery link: 
-<a href='https://seredakiryl.github.io/Cards/#/SetNewPassword/$token$'>
-link</a>
+export let messageStyle = `<div style="background-color: lightblue; padding: 15px">
+<h1>Trouble signing in?</h1><br>
+Resetting your password is easy.<br><br>
+Just press the button below and follow the instructions. We’ll have you up and running in no time.<br><br>
+<a style="display: inline-block;
+    background: #8C959D;
+    color: #fff;
+    padding: 1rem 1.5rem;
+    text-decoration: none;
+    border-radius: 3px;"
+     href='https://seredakiryl.github.io/Cards/#/set-new-password/$token$'
+     >Change Password</a><br><br>
+If you did not make this request then please ignore this email.
 </div>`
