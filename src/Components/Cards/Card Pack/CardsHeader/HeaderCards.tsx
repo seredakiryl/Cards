671d7193e1514c88436e26e7174a29f21235bb53
Anatoli Nikolaevich <tolya.tar@gmail.com>
2022-09-07 08:20:47 +0300

    add card btn modal created without thunk

--- src/Components/Cards/Card Pack/CardsHeader/HeaderCards.tsx
+++ src/Components/Cards/Card Pack/CardsHeader/HeaderCards.tsx
@@ -1,25 +1,44 @@
-import { ArrowLeftOutlined } from '@ant-design/icons'
-import { Button } from 'antd'
-
+import {Button, Modal} from 'antd'
 import { BackArrow } from '../../../BackArrow/BackArrow'
-
+import {Title} from "../../../Profile/Title/Title";
+import React, {useState} from "react";
 import s from './HeaderCards.module.css'
 
 type PropsType = {
   cardsPackId: string
 }
 export const HeaderCards = (props: PropsType) => {
-  const onclickHandlerAddNewCard = () => {
-    console.log('Create New Card in cardsPackId')
-  }
+
+    const [isAddCardModalVisible, setAddCardModalVisible] = useState(false)
+
+    const showAddCardModal = () => {
+        setAddCardModalVisible(true)
+    }
+
+    const handleAddCardOk = () => {
+        setAddCardModalVisible(false)
+
+    }
+    const handleCancel = () => {
+        setAddCardModalVisible(false)
+    }
 
   return (
     <div className={s.wrapper}>
-      <BackArrow url={'/packs'} />
-      <h2>Name Pack</h2>
-      <Button type="primary" shape="round" size={'middle'}>
-        Add new card
-      </Button>
+        <BackArrow url={'/packs'} />
+        <div className={s.inner}>
+            <Title text={'Name Pack'} />
+            <div>
+                <Button type="primary" onClick={showAddCardModal}>Add new card</Button>
+                <Modal
+                    title="Add new card"
+                    visible={isAddCardModalVisible}
+                    onOk={handleAddCardOk}
+                    onCancel={handleCancel}
+                >
+                </Modal>
+            </div>
+        </div>
     </div>
   )
 }
